765bee4e78f69087fd8dfd697ecfdeac6914f148
NizamiQarayevWork <nizamiq@maestro.st>
2026-03-01 21:49:55 +0400

    kmp fix

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -29,8 +29,10 @@ kotlin {
     }
 
     targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
-        compilations.getByName("main").cinterops.all {
-            extraOpts("-compiler-options", "-fno-modules")
+        compilations.configureEach {
+            cinterops.configureEach {
+                extraOpts("-compiler-options", "-fno-modules")
+            }
         }
     }
 
